baa7cecdbefb854cd56b82d95028a482fbc61a20
master_chef_2811 <assassinnyt209@gmail.com>
2024-03-22 14:35:25 +0700

    open minimumzoom

--- src/viewer.js
+++ src/viewer.js
@@ -980,7 +980,7 @@ class VolumeImageViewer {
       // resolutions: mapViewResolutions,
       constrainResolution: true,
       rotation: this[_rotation],
-      constrainOnlyCenter: false,
+      constrainOnlyCenter: true,
       smoothResolutionConstraint: true,
       showFullExtent: true,
       extent: this[_pyramid].extent
